e2e98641923e172db5f6b726034e88b006e9a885
Christian Dinglasan <65063750+christianjmd@users.noreply.github.com>
2020-08-06 15:31:21 +1000

    Add files via upload

---
+++ WalkThrough_files/script.js
@@ -0,0 +1,20 @@
+
+window.sr = ScrollReveal({duration: 6000, reset: true});
+sr.reveal('h1', {});
+sr.reveal('h2', {});
+sr.reveal('img', {});
+sr.reveal('wrapper row1', {});
+
+
+// onmouseover
+
+function setNewImage1(){
+  this.setAttribute("src", "images/demo/ok.gif");
+}
+
+function setOldImage1(){
+  this.setAttribute("src", "images/demo/loll.gif");
+}
+
+
+//carousel
